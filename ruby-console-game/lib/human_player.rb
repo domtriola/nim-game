7d91bfcce43778337d580f6786cd6acbcc2645c8
Dominick Triola <dominick@ignovadesign.com>
2016-08-06 14:15:56 -0700

    add input flexibility

--- ruby-console-game/lib/human_player.rb
+++ ruby-console-game/lib/human_player.rb
@@ -2,7 +2,7 @@ class HumanPlayer
   def get_move
     puts "Which row would you like to take from?
           And how many beans would you like to take? (row, beans)"
-    input = gets.chomp.split(", ")
+    input = gets.chomp.split(/,\s|,/)
     row = input[0].ord - 97
     beans = input[1].to_i
     [row, beans]
