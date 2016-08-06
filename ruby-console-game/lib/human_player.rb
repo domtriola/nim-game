5e7f6ae8cedd32c44661dd0d0b131867109fca4c
Dominick Triola <dominick@ignovadesign.com>
2016-08-06 12:47:35 -0700

    create human_player and corresponding tests

--- ruby-console-game/lib/human_player.rb
+++ ruby-console-game/lib/human_player.rb
@@ -1,2 +1,18 @@
 class HumanPlayer
+  def get_move
+    puts "Which row would you like to take from?
+          And how many beans would you like to take? (row, beans)"
+    input = gets.chomp.split(", ")
+    row = input[0].ord - 97
+    beans = input[1].to_i
+    [row, beans]
+  end
+
+  def display(board)
+    board.rows.each_with_index do |row, index|
+      beans = []
+      row.times { beans << "O" }
+      puts "#{(index + 97).chr}: #{beans.join(" ")}"
+    end
+  end
 end
---
+++ ruby-console-game/spec/human_player_spec.rb
@@ -0,0 +1,59 @@
+require "game"
+
+describe HumanPlayer do
+  let(:human) { HumanPlayer.new }
+  let(:board) { Board.new([3,4,5]) }
+
+  class NoMoreInput < StandardError
+  end
+
+  before do
+    $stdout = StringIO.new
+    $stdin = StringIO.new
+
+    class HumanPlayer
+      def gets
+        result = $stdin.gets
+        raise NoMoreInput unless result
+        result
+      end
+    end
+
+    def recent_output
+      outputs = $stdout.string.split("\n")
+      max = [outputs.length, 8].min
+      outputs[-max..-1].join(" ")
+    end
+
+    def human.get_move!
+      get_move rescue NoMoreInput
+    end
+  end
+
+  after :all do
+    $stdout = STDOUT
+    $stdin = STDIN
+  end
+
+  describe "#get_move" do
+    it "asks player which row they would like to take from, and how many beans" do
+      human.get_move!
+      expect($stdout.string.downcase).to match(/row/)
+      expect($stdout.string.downcase).to match(/beans/)
+    end
+
+    it "takes a character for the row and number of beans to take" do
+      $stdin.string << "a, 3"
+      expect(human.get_move).to eq([0, 3])
+    end
+  end
+
+  describe "#display" do
+    it "prints the rows to the console" do
+      human.display(board)
+      expect($stdout.string).to match(/a: O O O/)
+      expect($stdout.string).to match(/b: O O O O/)
+      expect($stdout.string).to match(/c: O O O O O/)
+    end
+  end
+end
