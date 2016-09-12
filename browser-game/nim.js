0bf5bc96283af1dc7b0ded398de22396a8449ad9
Dominick Triola <dominick@ignovadesign.com>
2016-09-12 01:40:29 -0700

    setup human player interface

--- browser-game/nim.js
+++ browser-game/nim.js
@@ -16,16 +16,40 @@ function elt(type, className) {
 }
 
 function DOMDisplay(parent, board) {
-  this.display = parent.appendChild(elt('div', 'board'));
+  this.board = parent.appendChild(elt('div'));
+  this.board.id = 'board';
   board.forEach(function(row) {
     var tokens = elt('div', 'row');
     for (var i = 0; i < row; i++) {
       var token = elt('div', 'token');
+      token.setAttribute('data-chosen', false);
+      token.addEventListener('click', function() {
+        var isTrue = this.getAttribute('data-chosen') === 'true';
+        if (isTrue)
+          this.setAttribute('data-chosen', 'false');
+        else
+          this.setAttribute('data-chosen', 'true');
+      });
       tokens.appendChild(token);
     }
-    this.display.appendChild(tokens);
+    this.board.appendChild(tokens);
+  });
+
+  this.controls = parent.appendChild(elt('div'));
+  var move = this.controls.appendChild(elt('button'));
+  move.innerHTML = 'move';
+  move.addEventListener('click', function() {
+    var board = document.getElementById('board');
+    for (var i = board.children.length - 1; i >= 0; i--) {
+      for (var j = board.children[i].children.length - 1; j >= 0; j--) {
+        var token = board.children[i].children[j];
+        var chosen = token.getAttribute('data-chosen') === 'true';
+        if (chosen)
+          token.parentNode.removeChild(token);
+      }
+    }
   });
 }
 DOMDisplay.prototype.clear = function() {
-  this.display.parent.removeChild(this.display);
+  this.board.parent.removeChild(this.board);
 }
