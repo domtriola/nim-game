a81705773c6f4cd7574447562c2fee1c162ee59e
Dominick Triola <dominick@ignovadesign.com>
2016-09-14 23:04:20 -0700

    create players

--- browser-game/nim.js
+++ browser-game/nim.js
@@ -1,13 +1,3 @@
-function Board(rows) {
-  this.rows = rows;
-}
-Board.prototype.removeFromRow = function(move) {
-  this.rows[move[0]] -= move[1];
-};
-Board.prototype.addToRow = function(move) {
-  this.rows[move[0]] += move[1];
-};
-
 function elt(type, className) {
   var element = document.createElement(type);
   if (className)
@@ -15,7 +5,15 @@ function elt(type, className) {
   return element;
 }
 
-function DOMDisplay(parent, board) {
+function Game(parent, board, playerOne, playerTwo) {
+  this.playerOne = playerOne;
+  this.playerTwo = playerTwo;
+  this.currentPlayer = this.playerOne;
+  this.parent = parent;
+  var self = this;
+
+  this.playerTurn = parent.appendChild(elt('h2'));
+
   this.board = parent.appendChild(elt('div'));
   this.board.id = 'board';
   board.forEach(function(row) {
@@ -32,13 +30,14 @@ function DOMDisplay(parent, board) {
       });
       tokens.appendChild(token);
     }
-    this.board.appendChild(tokens);
+    self.board.appendChild(tokens);
   });
 
   this.controls = parent.appendChild(elt('div'));
-  var move = this.controls.appendChild(elt('button'));
-  move.innerHTML = 'move';
-  move.addEventListener('click', function() {
+  this.controls.id = 'controls';
+  var moveButton = this.controls.appendChild(elt('button'));
+  moveButton.innerHTML = 'move';
+  moveButton.addEventListener('click', function() {
     var board = document.getElementById('board');
     for (var i = board.children.length - 1; i >= 0; i--) {
       for (var j = board.children[i].children.length - 1; j >= 0; j--) {
@@ -48,8 +47,39 @@ function DOMDisplay(parent, board) {
           token.parentNode.removeChild(token);
       }
     }
+    self.switchPlayers();
   });
 }
-DOMDisplay.prototype.clear = function() {
-  this.board.parent.removeChild(this.board);
+Game.prototype.switchPlayers = function() {
+  if (this.currentPlayer == this.playerOne)
+    this.currentPlayer = this.playerTwo;
+  else
+    this.currentPlayer = this.playerOne;
+  this.play();
+};
+Game.prototype.play = function() {
+  this.currentPlayer.takeTurn(this.playerTurn, this.controls);
+  if (this.over()) {
+    var winText = this.parent.appendChild(elt("h4"));
+    winText.innerHTML = currentPlayer.name + " Wins!";
+  }
+};
+Game.prototype.over = function() {
+  
+};
+
+function HumanPlayer(name) {
+  this.name = name;
 }
+HumanPlayer.prototype.takeTurn = function(playerTurn, controls) {
+  playerTurn.innerHTML = this.name + "'s Turn";
+  controls.style = 'display: block';
+};
+
+function ComputerPlayer(name) {
+  this.name = name;
+}
+ComputerPlayer.prototype.takeTurn = function(playerTurn, controls) {
+  playerTurn.innerHTML = this.name + "'s Turn";
+  controls.style = 'display: none';
+};
