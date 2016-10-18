bc0702c25aa277742bdc76153636053890419aee
Dominick Triola <dominick@ignovadesign.com>
2016-10-18 16:29:53 -0700

    setup basic board

--- react-native-game/Nim/index.ios.js
+++ react-native-game/Nim/index.ios.js
@@ -16,16 +16,24 @@ export default class Nim extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={styles.row}>
+          <View style={styles.token}></View>
+          <View style={styles.token}></View>
+          <View style={styles.token}></View>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.token}></View>
+          <View style={styles.token}></View>
+          <View style={styles.token}></View>
+          <View style={styles.token}></View>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.token}></View>
+          <View style={styles.token}></View>
+          <View style={styles.token}></View>
+          <View style={styles.token}></View>
+          <View style={styles.token}></View>
+        </View>
       </View>
     );
   }
@@ -38,15 +46,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  row: {
+    flexDirection: 'row',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  token: {
+    width: 50,
+    height: 50,
+    margin: 10,
+    backgroundColor: 'teal',
   },
 });
 
