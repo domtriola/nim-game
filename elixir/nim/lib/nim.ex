ceb090688dc292741c791d185074153c657e7263
Dominick Triola <dominick@ignovadesign.com>
2017-03-05 18:05:06 -0800

    initialize elixir nim

---
+++ elixir/nim/lib/nim.ex
@@ -0,0 +1,18 @@
+defmodule Nim do
+  @moduledoc """
+  Documentation for Nim.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nim.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/nim/test/nim_test.exs
@@ -0,0 +1,4 @@
+defmodule NimTest do
+  use ExUnit.Case
+  doctest Nim
+end
